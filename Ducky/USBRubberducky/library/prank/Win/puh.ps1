bb5901e8b1fff662fa182c0241e6fd7058406f8c
FalsePhilosopher <108245223+FalsePhilosopher@users.noreply.github.com>
2022-09-26 18:14:25 -0600

    Delete puh.ps1

--- Ducky/USBRubberducky/library/prank/Win/puh.ps1
+++
@@ -1,7 +0,0 @@
-Add-Type -a system.windows.forms
-foreach($i in 1..9999)
-{
-$x=Get-Random -Min 10 -Max 1910
-$y=Get-Random -Min 10 -Max 1070
-[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x,$y)
-}

